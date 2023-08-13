c9276dbaa08f655099cb6a846b580656d2780b4b
Tarek Tarho <tarek_tarho@hotmail.com>
2023-08-14 01:03:51 +0200

    Style

--- src/app/[lang]/components/Header.tsx
+++ src/app/[lang]/components/Header.tsx
@@ -4,7 +4,7 @@ import LocaleSwitcher from './LocaleSwitcher'
 const Header: React.FC = () => {
 
   return (
-    <header className="header">
+    <header className="flex flex-wrap justify-between items-center mx-auto p-10 header">
       <nav className="container flex items-center justify-between">
         <div className="container flex">
           <h1>Tag Managment System</h1>
