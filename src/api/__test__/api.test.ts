84d167493c83e1ebdf02d412b408daf75896f7be
Tarek Tarho <tarek_tarho@hotmail.com>
2023-08-15 10:43:09 +0200

    Code improvements & Adding extra test

--- src/api/__test__/api.test.ts
+++ src/api/__test__/api.test.ts
@@ -1,16 +1,13 @@
 import * as API from '../api'
 import fetchMock from "jest-fetch-mock"
 import { ITag } from "../../types/tags"
-import { BASE_URL, ERROR_INVALID_TAG } from "@/utils/constants"
+import { BASE_URL, ERROR_INVALID_TAG, STATUS_OK } from "@/utils/constants"
 import { STATUS_CREATED } from "../../utils/constants"
 
 // Mock the global fetch function
 fetchMock.enableMocks()
 
 describe('API Tests', () => {
-  beforeEach(() => {
-    fetchMock.resetMocks()
-  })
 
   const mockTags: ITag[] = [
     { id: '1', name: 'Tag 1', deleted: false, createdAt: '2023-08-12T09:47:36.247Z' },
@@ -18,6 +15,10 @@ describe('API Tests', () => {
   ]
 
   describe('getAllTags', () => {
+    afterEach(() => {
+      fetchMock.resetMocks()
+      fetchMock.mockClear()
+    })
     it('fetches successfully data from an API', async () => {
       // Mock the fetch response
       fetchMock.mockResponseOnce(JSON.stringify(mockTags))
@@ -35,23 +36,40 @@ describe('API Tests', () => {
     })
 
     it('throws an error when fetch fails', async () => {
-      // Mock the fetch response to throw an error
-      fetchMock.mockRejectedValueOnce(new Error('Network error'))
+      const errorMessage = 'Unknown Error'
+      // Modify the fetch mock to simulate an unknown error
+      fetchMock.mockRejectedValueOnce(new Error(errorMessage))
 
-      await expect(API.getAllTags()).rejects.toThrow('Network error')
+      // Expect that calling getAllTags will throw an unknown error message
+      await expect(API.getAllTags()).rejects.toThrow(
+        `An error occurred while fetching tags: ${errorMessage}`
+      )
     })
+
+    it('throws an unknown error for non-Error object', async () => {
+      const errorMessage = 'An unknown error occurred while fetching tags'
+      // Mock the fetch response to throw an error
+      fetchMock.mockRejectedValueOnce(errorMessage)
+      await expect(API.getAllTags()).rejects.toThrow(errorMessage)
+    })
+
   })
 
   describe('addTag', () => {
+    afterEach(() => {
+      fetchMock.resetMocks()
+      fetchMock.mockClear()
+    })
+
+    const newTag: ITag = {
+      id: 'new-tag-id',
+      name: 'New Tag',
+      deleted: false,
+      createdAt: '2023-08-12T09:47:36.247Z',
+    }
+
     it('adds a tag successfully', async () => {
-      const newTag: ITag = {
-        id: 'new-tag-id',
-        name: 'New Tag',
-        deleted: false,
-      }
-
       const mockResponse = { ...newTag }
-
       // Mock the fetch response for a successful POST request
       fetchMock.mockResponse(JSON.stringify(mockResponse), {
         status: STATUS_CREATED, // Status code for successful creation
@@ -77,13 +95,6 @@ describe('API Tests', () => {
       // Mock the fetch response for a failed POST request
       fetchMock.mockRejectedValueOnce(new Error('Failed to add tag'))
 
-      const newTag: ITag = {
-        id: 'new-tag-id',
-        name: 'New Tag',
-        deleted: false,
-        createdAt: '2023-08-12T09:47:36.247Z',
-      }
-
       await expect(API.addTag(newTag)).rejects.toThrow('Failed to add tag')
       expect(fetchMock).toHaveBeenCalledWith(
         `${BASE_URL}/tags`,
@@ -102,5 +113,135 @@ describe('API Tests', () => {
         ERROR_INVALID_TAG,
       )
     })
+
+    it('throws an error for non-OK response status', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify({}), {
+        status: 400
+      })
+      // Expect that calling addTag will throw an error
+      await expect(API.addTag(newTag)).rejects.toThrowError(
+        'Request failed with the following status code: 400'
+      )
+    })
+
   })
+
+  describe('editTag', () => {
+
+    afterEach(() => {
+      fetchMock.mockClear()
+      fetchMock.resetMocks()
+    })
+    const mockTag = { id: '1', name: 'Updated Tag', deleted: false } // Mock tag data
+
+    it('throws an error when tag is invalid', async () => {
+      const invalidTag = { id: '1', name: '', deleted: false } // Invalid tag with empty name
+      await expect(API.editTag(invalidTag)).rejects.toThrow(ERROR_INVALID_TAG)
+    })
+
+    it('returns the updated tag when the request is successful', async () => {
+      // Mock the fetch function to return the mockResponse
+      fetchMock.mockResponse(JSON.stringify(mockTag), {
+        status: STATUS_OK, // Status code for successful creation
+      })
+
+
+      // Call the editTag function and expect the result to match the mockTag data
+      const result = await API.editTag(mockTag)
+      expect(result).toEqual(mockTag)
+
+      // Verify that fetch was called with the correct URL and options
+      expect(global.fetch).toHaveBeenCalledWith(`${BASE_URL}/tags/${mockTag.id}`, {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(mockTag),
+      })
+    })
+
+    it('throws an error when the request fails', async () => {
+      // Mock the fetch function to simulate a rejected promise
+      fetchMock.mockRejectedValueOnce(new Error('Request failed'))
+
+      // Expect the editTag function to throw an error
+      await expect(API.editTag(mockTag)).rejects.toThrow('Request failed')
+
+      // Verify that fetch was called with the correct URL and options
+      expect(global.fetch).toHaveBeenCalledWith(`${BASE_URL}/tags/${mockTag.id}`, {
+        method: 'PUT',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(mockTag),
+      })
+    })
+
+    it('throws an error for non-OK response status', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify(mockTag), {
+        status: 400
+      })
+      await expect(API.editTag(mockTag)).rejects.toThrow('Request failed with status code: 400')
+    })
+
+    it('throws an error when edit fails with custom error message', async () => {
+      const errorMessage = 'Unknown Error'
+      // Modify the fetch mock to simulate an unknown error
+      fetchMock.mockRejectedValueOnce(new Error(errorMessage))
+
+      // Expect that calling getAllTags will throw an unknown error message
+      await expect(API.editTag(mockTag)).rejects.toThrow(
+        `An error occurred while editing the tag: ${errorMessage}`
+      )
+    })
+
+    it('throws an unknown error for non-Error object', async () => {
+      const errorMessage = 'An unknown error occurred while editing tags'
+      // Mock the fetch response to throw an error
+      fetchMock.mockRejectedValueOnce(errorMessage)
+      await expect(API.editTag(mockTag)).rejects.toThrow(errorMessage)
+    })
+
+  })
+
+
+  describe('deleteTag function', () => {
+    afterEach(() => {
+      fetchMock.mockClear()
+      fetchMock.resetMocks()
+    })
+
+    const mockTag = { id: '1', name: 'Updated Tag', deleted: false } // Mock tag data
+
+    it('deletes a tag successfully', async () => {
+
+      fetchMock.mockResponse(JSON.stringify(mockTag), {
+        status: STATUS_OK,
+        headers: { 'Content-Type': 'application/json' }
+      })
+      API.deleteTag(mockTag.id)
+      expect(fetchMock).toHaveBeenCalledWith(
+        `${BASE_URL}/tags/${mockTag.id}`,
+        expect.objectContaining({
+          method: 'DELETE',
+        })
+      )
+    })
+
+    it('throws an error when deletion fails', async () => {
+
+      fetchMock.mockResponse(JSON.stringify(mockTag), {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' }
+      })
+
+      await expect(API.deleteTag(mockTag.id)).rejects.toThrow(
+        `Failed to delete tag. Status code: 404`
+      )
+    })
+
+    it('throws an error when an unknown error occurs', async () => {
+      const errorMessage = 'An unknown error occurred while deleting tags'
+      // Mock the fetch response to throw an error
+      fetchMock.mockRejectedValueOnce(errorMessage)
+      await expect(API.deleteTag(mockTag.id)).rejects.toThrow(errorMessage)
+    })
+  })
+
 })
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import { ITag } from "@/types/tags"
-import { STATUS_OK, BASE_URL, ERROR_INVALID_TAG } from "../utils/constants"
+import { BASE_URL, ERROR_INVALID_TAG } from "../utils/constants"
 
 /**
  * Fetches all tags from the server.
@@ -27,7 +27,7 @@ export const getAllTags = async (): Promise<ITag[]> => {
 }
 
 /**
- * Adds a new tag to the server.
+ * Adds a new tag
  * @param {ITag} tag - The tag object to be added.
  * @returns {Promise<ITag>} A promise that resolves to the added tag object.
  */
@@ -57,13 +57,14 @@ export const addTag = async (tag: ITag): Promise<ITag> => {
 }
 
 /**
- * Edits an existing tag on the server.
+ * Edits an existing tag
  * @param {ITag} tag - The tag object to be edited.
  * @returns {Promise<ITag>} A promise that resolves to the edited tag object.
  */
 export const editTag = async (tag: ITag): Promise<ITag> => {
   const name = tag.name.trim()
-  if (!tag || name.length === 0) {
+
+  if (name.length === 0) {
     throw new Error(ERROR_INVALID_TAG)
   }
 
@@ -73,29 +74,44 @@ export const editTag = async (tag: ITag): Promise<ITag> => {
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify(tag)
     })
-    const responseData = await response.json()
 
-    if (response.status === STATUS_OK) {
-      return responseData
+    if (!response.ok) {
+      throw new Error(`Request failed with status code: ${response.status}`)
     }
 
-    throw responseData
+    return response.json()
   } catch (error) {
-    throw error
+    if (error instanceof Error) {
+      throw new Error(`An error occurred while editing the tag: ${error.message}`)
+    } else {
+      throw new Error(`An unknown error occurred while editing tags`)
+    }
   }
 }
 
+
 /**
- * Deletes a tag from the server.
+ * Deletes a tag
  * @param {string} id - The ID of the tag to be deleted.
  * @returns {Promise<void>} A promise that resolves when the tag is deleted.
  */
 export const deleteTag = async (id: string): Promise<void> => {
+  const url = `${BASE_URL}/tags/${id}`
+
   try {
-    await fetch(`${BASE_URL}/tags/${id}`, {
+    const response = await fetch(url, {
       method: 'DELETE',
     })
+
+    if (!response.ok) {
+      throw new Error(`Failed to delete tag. Status code: ${response.status}`)
+    }
   } catch (error) {
-    throw error
+    if (error instanceof Error) {
+      throw new Error(`An error occurred while deleting the tag: ${error.message}`)
+    } else {
+      throw new Error(`An unknown error occurred while deleting tags`)
+    }
   }
-}
+};
+
