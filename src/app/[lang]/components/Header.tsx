c3ab91b5b4b8e3deb84d28265bc072906108e554
TarekTarho <tarek_tarho@hotmail.com>
2023-10-09 09:22:53 -0400

    Implement Logout

--- src/app/[lang]/components/Header.tsx
+++ src/app/[lang]/components/Header.tsx
@@ -1,16 +1,21 @@
 "use client"
-import LocaleSwitcher from './LocaleSwitcher'
+import LocaleSwitcher from "./LocaleSwitcher"
+import { signOut } from "next-auth/react"
 
 const Header: React.FC = () => {
-
   return (
     <header className="flex flex-wrap justify-between items-center mx-auto p-10 header">
       <nav className="container flex items-center justify-between">
-        <div className="container flex">
+        <div className="container flex items-center">
           <h1>Tag Managment System</h1>
+          {/* Include the LocaleSwitcher component to allow language switching */}
+          <LocaleSwitcher />
         </div>
-        {/* Include the LocaleSwitcher component to allow language switching */}
-        <LocaleSwitcher />
+        <div className="btn-container">
+          <button onClick={() => signOut()} className="rounded-md border bg-black px-3 py-2 text-white glass">
+            Logout
+          </button>
+        </div>
       </nav>
     </header>
   )
