d84fcf7d236e4727e9e7660c6fcabbed5c9cdb28
TarekTarho <tarek_tarho@hotmail.com>
2023-10-10 20:30:39 -0400

    Add logs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,18 +1,25 @@
+import * as Sentry from "@sentry/browser"
+
+Sentry.init({
+  dsn: "https://f3bd82cf6bdb1a3646ad6df9a894f7e9@o4506017367654400.ingest.sentry.io/4506017370144768",
+})
 export const getCurrentTimestamp = (): number => {
   return Math.floor(Date.now() / 1000)
 }
 
 export const formatTimestampToHumanReadable = (timestamp: number): string => {
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
     hour: "numeric",
     minute: "numeric",
-    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone, // Get current time zone
+    timeZone: timeZone, // Get current time zone
   }
 
   const formattedDate = new Date(timestamp * 1000).toLocaleDateString("en-GB", options)
+  Sentry.captureMessage(`${timeZone} - timeZone`)
   return formattedDate
 }
 
